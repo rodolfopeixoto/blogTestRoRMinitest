86a8a228c6a7d72cf0e4548770d79affe08bf218
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2018-07-13 09:45:33 -0300

    Create fixtures

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,16 +1,28 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
+  # test "should not save article without title" do
+  #   article = Article.new
+  #   assert_not article.save, "Saved the article without a title"
+  # end
+
+  # test "should sum" do
+  #   assert_equal 4, 2+2
+  # end
+
+  # test "should not sum" do
+  #   assert_not_equal 4, 3+2
+  # end
   test "should not save article without title" do
     article = Article.new
     assert_not article.save, "Saved the article without a title"
   end
 
-  test "should sum" do
-    assert_equal 4, 2+2
+  test "should do some interesting with stored state" do
+    assert_equal "My first post", articles(:one).title
   end
 
-  test "should not sum" do
-    assert_not_equal 4, 3+2
+  test "should to return amount" do
+    assert_equal 1002, Article.count
   end
 end
